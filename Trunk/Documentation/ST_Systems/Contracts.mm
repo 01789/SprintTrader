f29f42231d45f4e38e441ef798ad17b4c1806a1b
AJ Twombly <AndrewJTwombly@gmail.com>
2014-04-24 16:56:08 -0400

    Work on some more mind maps [11195529]
    Signed-off-by: AJ Twombly <AndrewJTwombly@gmail.com>

---
+++ Trunk/Documentation/ST_Systems/Contracts.mm
@@ -0,0 +1,448 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1398360848055" ID="ID_1037010211" MODIFIED="1398365085444" TEXT="Gain a Contract">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1398362922474" ID="ID_199515249" MODIFIED="1398365085441" POSITION="right" TEXT="Contract generation algorithm">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1398362938795" ID="ID_1440571245" MODIFIED="1398365358940">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mission Style
+    </p>
+    <ul>
+      <li>
+        Cargo
+      </li>
+      <li>
+        Bounty<br />
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1398362963436" ID="ID_1160168111" MODIFIED="1398365362157">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Legality
+    </p>
+    <ul>
+      <li>
+        Alliance sanctioned
+      </li>
+      <li>
+        Legal<br />
+      </li>
+      <li>
+        Grey
+      </li>
+      <li>
+        Illegal
+      </li>
+      <li>
+        Treasonous<br />
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1398362973500" ID="ID_353026215" MODIFIED="1398365392037">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Assigning Organization
+    </p>
+    <ul>
+      <li>
+        Alliance
+      </li>
+      <li>
+        Private corporation
+      </li>
+      <li>
+        Pirates
+      </li>
+      <li>
+        Organized crime
+      </li>
+      <li>
+        Citizens
+      </li>
+      <li>
+        Other
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1398363488562" ID="ID_1404409092" MODIFIED="1398365415718">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Reputation
+    </p>
+    <ul>
+      <li>
+        Tier 1
+      </li>
+      <li>
+        Tier 2
+      </li>
+      <li>
+        Tier 3
+      </li>
+      <li>
+        Bribe
+      </li>
+      <li>
+        &quot;I know a guy&quot;
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1398363023199" ID="ID_1356812659" MODIFIED="1398365442472">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Size and shape
+    </p>
+    <ul>
+      <li>
+        Bulky
+      </li>
+      <li>
+        Heavy
+      </li>
+      <li>
+        Volatile
+      </li>
+      <li>
+        Delicate
+      </li>
+      <li>
+        Light
+      </li>
+      <li>
+        Small
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1398281497786" ID="ID_404591899" MODIFIED="1398364016104" TEXT="Merchant Goods">
+<node COLOR="#111111" CREATED="1398281505001" ID="ID_993681360" MODIFIED="1398283296365" TEXT="Food"/>
+<node COLOR="#111111" CREATED="1398281509128" ID="ID_540790171" MODIFIED="1398283296366" TEXT="Home Appliances"/>
+<node COLOR="#111111" CREATED="1398281511432" ID="ID_837930676" MODIFIED="1398283296366" TEXT="Clothing"/>
+<node COLOR="#111111" CREATED="1398281512616" ID="ID_1634808455" MODIFIED="1398283296367" TEXT="Vehicles"/>
+<node COLOR="#111111" CREATED="1398281513800" ID="ID_375006981" MODIFIED="1398283296368" TEXT="Scrap metal"/>
+<node COLOR="#111111" CREATED="1398281515865" ID="ID_407916310" MODIFIED="1398283296368" TEXT="Computer Parts"/>
+<node COLOR="#111111" CREATED="1398281518184" ID="ID_844413496" MODIFIED="1398283296369" TEXT="Machine Parts"/>
+<node COLOR="#111111" CREATED="1398281522168" ID="ID_354893776" MODIFIED="1398283296369" TEXT="Electronics"/>
+</node>
+<node COLOR="#111111" CREATED="1398281524840" ID="ID_125859486" MODIFIED="1398283296369" TEXT="People">
+<node COLOR="#111111" CREATED="1398281531272" ID="ID_796368066" MODIFIED="1398283296370" TEXT="Dignitaries"/>
+<node COLOR="#111111" CREATED="1398281533529" ID="ID_1920928865" MODIFIED="1398283296370" TEXT="Messangers"/>
+<node COLOR="#111111" CREATED="1398281539449" ID="ID_1953602530" MODIFIED="1398283296370" TEXT="Families"/>
+<node COLOR="#111111" CREATED="1398281541913" ID="ID_1894827330" MODIFIED="1398283296371" TEXT="Refugees"/>
+<node COLOR="#111111" CREATED="1398281707937" ID="ID_1444387210" MODIFIED="1398283296371" TEXT="Vacationers"/>
+</node>
+<node COLOR="#111111" CREATED="1398281548330" ID="ID_1835208581" MODIFIED="1398283296371" TEXT="Finery">
+<node COLOR="#111111" CREATED="1398281565675" ID="ID_1219780969" MODIFIED="1398283296372" TEXT="Designer clothes"/>
+<node COLOR="#111111" CREATED="1398281568858" ID="ID_341084763" MODIFIED="1398283296372" TEXT="Jewelry"/>
+<node COLOR="#111111" CREATED="1398281571130" ID="ID_91228290" MODIFIED="1398283296372" TEXT="Fine fabrics"/>
+<node COLOR="#111111" CREATED="1398281574618" ID="ID_763332255" MODIFIED="1398283296373" TEXT="Luxury goods"/>
+</node>
+<node COLOR="#111111" CREATED="1398281550745" ID="ID_504082242" MODIFIED="1398283296373" TEXT="Volatile &amp; Fragile Cargo">
+<node COLOR="#111111" CREATED="1398281625500" ID="ID_268014137" MODIFIED="1398283296373" TEXT="Petroleum Fuel"/>
+<node COLOR="#111111" CREATED="1398281633165" ID="ID_891309979" MODIFIED="1398283296374" TEXT="Compressed Gasses"/>
+<node COLOR="#111111" CREATED="1398281649534" ID="ID_352727638" MODIFIED="1398283296375" TEXT="Starship Fuel"/>
+<node COLOR="#111111" CREATED="1398281677599" ID="ID_1844933400" MODIFIED="1398283296375" TEXT="Explosives"/>
+</node>
+<node COLOR="#111111" CREATED="1398364019257" ID="ID_1894320440" MODIFIED="1398365085444" TEXT="Fragile Cargo">
+<node COLOR="#111111" CREATED="1398281642701" ID="ID_685983197" MODIFIED="1398283296374" TEXT="Scientific Equipment"/>
+<node COLOR="#111111" CREATED="1398281681279" ID="ID_1302758338" MODIFIED="1398283296375" TEXT="Advanced Laser Lenses"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1398360874304" ID="ID_1571412150" MODIFIED="1398365085422" POSITION="right" TEXT="Gain a contract procedure">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1398362285717" ID="ID_239779173" MODIFIED="1398365465536">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Location
+    </p>
+    <ul>
+      <li>
+        Dock with station
+      </li>
+      <li>
+        Land on/orbit a planet
+      </li>
+      <li>
+        Meet spacefaring mission giver
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1398364116877" ID="ID_609106658" MODIFIED="1398365510948">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Make contact
+    </p>
+    <ul>
+      <li>
+        Use reputation to talk to a contact
+      </li>
+      <li>
+        Use a previous contact to hook you up
+      </li>
+      <li>
+        Bribe locals to see where to hang out
+      </li>
+      <li>
+        Advertise services to the public
+      </li>
+      <li>
+        Sell services to a company
+      </li>
+      <li>
+        Contact local Alliance outpost
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1398364193633" ID="ID_1407372594" MODIFIED="1398365529098">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Browse
+    </p>
+    <ul>
+      <li>
+        <p>
+          Several contracts may be available. Maybe none from that particular source. Choose a contract, or none.
+        </p>
+      </li>
+      <li>
+        <p>
+          Refusing contracts that are labelled as crucial or vital (or something) may cause reputation reduction.
+        </p>
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1398364769098" ID="ID_895665468" MODIFIED="1398365550116">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Haggle
+    </p>
+    <ul>
+      <li>
+        Some contracts will allow haggling. Better reputation grants a better price on services.
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1398364775738" ID="ID_581000167" MODIFIED="1398365572901">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Accept
+    </p>
+    <ul>
+      <li>
+        <p>
+          Load cargo
+        </p>
+      </li>
+      <li>
+        <p>
+          Update map
+        </p>
+      </li>
+      <li>
+        <p>
+          Gain special mission-specific items
+        </p>
+      </li>
+      <li>
+        <p>
+          etc.
+        </p>
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1398364778428" ID="ID_1347503874" MODIFIED="1398365594518">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Begin
+    </p>
+    <ul>
+      <li>
+        Mission begins upon leaving the station, planet, or breaking contact with the starship.
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1398364877278" ID="ID_310835806" MODIFIED="1398365626303">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Adventure
+    </p>
+    <ul>
+      <li>
+        <p>
+          Carry out the mission parameters
+        </p>
+      </li>
+      <li>
+        <p>
+          Haul cargo
+        </p>
+      </li>
+      <li>
+        <p>
+          Find bounty
+        </p>
+      </li>
+      <li>
+        <p>
+          Destroy target
+        </p>
+      </li>
+      <li>
+        <p>
+          Search X for Y
+        </p>
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1398364916495" ID="ID_727162171" MODIFIED="1398365649122">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Finish
+    </p>
+    <ul>
+      <li>
+        <p>
+          Reach the end location
+        </p>
+      </li>
+      <li>
+        <p>
+          Return to original contract giver
+        </p>
+      </li>
+      <li>
+        <p>
+          Collect the agreed-upon money
+        </p>
+      </li>
+      <li>
+        <p>
+          Some less-than-reputable contract-givers may stiff you on your salary, either challenge or let it go.
+        </p>
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1398360913392" ID="ID_375695545" MODIFIED="1398365085434" POSITION="left" TEXT="Prerequisites to gaining contracts">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1398360920081" ID="ID_1426227325" MODIFIED="1398365085435" TEXT="Must be at location where Contract is offered">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1398360929041" ID="ID_654562756" MODIFIED="1398365085436" TEXT="Must not be Wanted by the offering organization">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1398360943473" ID="ID_1361741936" MODIFIED="1398365085437" TEXT="Must have access to the endzone of the contract">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1398360958306" ID="ID_712782049" MODIFIED="1398365085438" TEXT="Must be able to carry out the contract at the time of acceptance">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1398360981795" ID="ID_1474379707" MODIFIED="1398365085439" TEXT="Ample cargo space">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1398360985492" ID="ID_834553006" MODIFIED="1398365085439" TEXT="Proper mods to carry cargo">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1398361293984" ID="ID_273052550" MODIFIED="1398365085440" TEXT="Weapons/defenses for dangerous missions">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1398365662560" ID="ID_149514064" MODIFIED="1398365664973" TEXT="etc">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ Trunk/Documentation/ST_Systems/Ship Upgrades.mm
@@ -0,0 +1,104 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1398372417243" ID="ID_1070356044" MODIFIED="1398372447776" TEXT="Ship Upgrades">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1398281246893" ID="ID_377899719" MODIFIED="1398372447741" POSITION="left" TEXT="Cargo Bays">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1398285037255" ID="ID_190319723" MODIFIED="1398372447747" POSITION="left" TEXT="Chassis">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1398285042952" ID="ID_773396668" MODIFIED="1398372447749" TEXT="Small">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1398285046263" ID="ID_545121680" MODIFIED="1398372447750" TEXT="Medium">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1398285047752" ID="ID_981784168" MODIFIED="1398372447751" TEXT="Large">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1398281242509" ID="ID_1937552635" MODIFIED="1398372905936" POSITION="left" TEXT="Engines">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1398285057816" ID="ID_685973288" MODIFIED="1398372447753" POSITION="left" TEXT="Heat generation/absorption">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1398285096106" ID="ID_1898390645" MODIFIED="1398372447760" POSITION="right" TEXT="Mod Slots">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1398281250124" ID="ID_345762653" MODIFIED="1398372447761" TEXT="Shields">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1398285475065" ID="ID_248292166" MODIFIED="1398372447761" TEXT="Navigation shields">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1398285484954" ID="ID_887924161" MODIFIED="1398372447762" TEXT="Defensive shields">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1398285479370" ID="ID_189088738" MODIFIED="1398372447762" TEXT="Combat shields">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1398281251437" ID="ID_865989852" MODIFIED="1398372447764" TEXT="Weapons">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1398281252780" ID="ID_911985634" MODIFIED="1398372447764" TEXT="Stealth">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1398281254029" ID="ID_979684728" MODIFIED="1398372447765" TEXT="Smuggling">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1398281255581" ID="ID_932523007" MODIFIED="1398372447766" TEXT="Countermeasures">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1398281258397" ID="ID_1590120346" MODIFIED="1398372447768" TEXT="Repair">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1398281245196" ID="ID_453057086" MODIFIED="1398372917327" POSITION="right" TEXT="Navigation systems">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1398285083209" ID="ID_1361548777" MODIFIED="1398372910591" POSITION="right" TEXT="Reactors">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1398281038836" ID="ID_284275057" MODIFIED="1398372447771" POSITION="right" TEXT="Ship Upgrades">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1398281161881" ID="ID_640895666" MODIFIED="1398372447772" TEXT="Money">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1398281163225" ID="ID_203956416" MODIFIED="1398372447773" TEXT="Dry Docks">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1398281171065" ID="ID_170621219" MODIFIED="1398372447774" TEXT="Manufacturers">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1398372888772" ID="ID_319950966" MODIFIED="1398372893441" POSITION="right" TEXT="Weapons">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1398372894308" ID="ID_271356993" MODIFIED="1398372895296" POSITION="right" TEXT="Shields">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
