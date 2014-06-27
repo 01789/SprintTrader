9f543edf1ab1ef57c2a8ae44a468529d7df14cff
AJ Twombly <AndrewJTwombly@gmail.com>
2014-06-27 16:59:29 -0400

    Progress! (Finally)
    Signed-off-by: AJ Twombly <AndrewJTwombly@gmail.com>

---
+++ Trunk/Documentation/FlowChart_Contract Delivery.mm
@@ -0,0 +1,215 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1403894897692" ID="ID_1759324680" MODIFIED="1403894904422" TEXT="Contract Delivery">
+<node CREATED="1403894906380" ID="ID_735275080" MODIFIED="1403895028919" POSITION="right" TEXT="Dock screen">
+<node CREATED="1403894918582" ID="ID_731190128" MODIFIED="1403895022728" TEXT="Go to market">
+<node CREATED="1403895098051" ID="ID_220400025" MODIFIED="1403895108507" TEXT="Account for dock properties">
+<node CREATED="1403895110013" ID="ID_152646893" MODIFIED="1403895111066" TEXT="Size"/>
+<node CREATED="1403895111709" ID="ID_696119808" MODIFIED="1403895115227" TEXT="Position"/>
+<node CREATED="1403895115438" ID="ID_1051552421" MODIFIED="1403895116827" TEXT="Wealth"/>
+<node CREATED="1403895117054" ID="ID_437094977" MODIFIED="1403895125019" TEXT="Previous contracts completed"/>
+</node>
+<node CREATED="1403894993977" ID="ID_1284268617" MODIFIED="1403895011862" TEXT="Generate listings">
+<node CREATED="1403895001724" ID="ID_1916327070" MODIFIED="1403895048876" TEXT="Filter listings">
+<node CREATED="1403895048877" ID="ID_108287797" MODIFIED="1403895053517" TEXT="Scroll through available listings">
+<node CREATED="1403895053518" ID="ID_1364115959" MODIFIED="1403895201406" TEXT="Choose listing"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1403894927335" ID="ID_1827561222" MODIFIED="1403895014919" TEXT="Gather rumors">
+<node CREATED="1403895066460" ID="ID_967489535" MODIFIED="1403895071577" TEXT="Calculate reptuation">
+<node CREATED="1403895126990" ID="ID_1499000794" MODIFIED="1403895142333" TEXT="Allegiance (corporations or government)"/>
+<node CREATED="1403895142560" ID="ID_298610293" MODIFIED="1403895176382" TEXT="Contract fulfillment rate"/>
+<node CREATED="1403895149903" ID="ID_1815987333" MODIFIED="1403895171629" TEXT="Contract completion"/>
+</node>
+<node CREATED="1403895072957" ID="ID_528722962" MODIFIED="1403895079945" TEXT="Generate listings">
+<node CREATED="1403895178417" ID="ID_1806768350" MODIFIED="1403895183106" TEXT="Filter listings">
+<node CREATED="1403895183107" ID="ID_1382530221" MODIFIED="1403895188498" TEXT="Scroll through available listings">
+<node CREATED="1403895188499" ID="ID_1246132078" MODIFIED="1403895190994" TEXT="Choose listing">
+<node CREATED="1403895190995" ID="ID_1201460865" MODIFIED="1403895196727" TEXT="Bargain"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1403894987386" ID="ID_1822470707" MODIFIED="1403895017862" TEXT="Meet contact">
+<node CREATED="1403895205938" ID="ID_1218877703" MODIFIED="1403895214560" TEXT="Activate quest"/>
+<node CREATED="1403895215234" ID="ID_1789121369" MODIFIED="1403895219123" TEXT="Generate listing">
+<node CREATED="1403895219124" ID="ID_1867391168" MODIFIED="1403895224591" TEXT="Accept or decline"/>
+</node>
+</node>
+</node>
+<node CREATED="1403895312631" ID="ID_863620526" MODIFIED="1403895319959" POSITION="left" TEXT="Generate Listing">
+<node CREATED="1403895319959" ID="ID_1285986735" MODIFIED="1403895329272" TEXT="Determine source">
+<node CREATED="1403895329273" ID="ID_1594577105" MODIFIED="1403895332789" TEXT="Station"/>
+<node CREATED="1403895333544" ID="ID_348888845" MODIFIED="1403895334821" TEXT="Planet"/>
+<node CREATED="1403895335032" ID="ID_64781580" MODIFIED="1403895338693" TEXT="Ship"/>
+</node>
+<node CREATED="1403895252724" ID="ID_835012517" MODIFIED="1403895258725" TEXT="Determine if player is Wanted">
+<node CREATED="1403895258725" ID="ID_486089427" MODIFIED="1403895272946" TEXT="If True activate law enforcement">
+<node CREATED="1403895275461" ID="ID_1545677965" MODIFIED="1403895286118" TEXT="Begin silent countdown (measured in actions)"/>
+</node>
+</node>
+<node CREATED="1403895340423" ID="ID_734076502" MODIFIED="1403895350005" TEXT="Compare Reputation">
+<node CREATED="1403895351144" ID="ID_732300316" MODIFIED="1403895362998" TEXT="High rep grants better yield contracts"/>
+<node CREATED="1403895363625" ID="ID_670453859" MODIFIED="1403895379222" TEXT="Very low rep grants poor/dangerous contracts"/>
+<node CREATED="1403895379721" ID="ID_1201355164" MODIFIED="1403895418408" TEXT="Wanted yields only illegal contracts"/>
+</node>
+<node CREATED="1403895420299" ID="ID_1140560102" MODIFIED="1403895450685" TEXT="Roll on table for details">
+<node COLOR="#111111" CREATED="1398362938795" ID="ID_1440571245" MODIFIED="1398449567733">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mission Style
+    </p>
+    <ul>
+      <li>
+        Cargo
+      </li>
+      <li>
+        Bounty<br />
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1398362963436" ID="ID_1160168111" MODIFIED="1398449567734">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Legality
+    </p>
+    <ul>
+      <li>
+        Alliance sanctioned
+      </li>
+      <li>
+        Legal<br />
+      </li>
+      <li>
+        Grey
+      </li>
+      <li>
+        Illegal
+      </li>
+      <li>
+        Treasonous<br />
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1398362973500" ID="ID_353026215" MODIFIED="1398449567736">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Assigning Organization
+    </p>
+    <ul>
+      <li>
+        Alliance
+      </li>
+      <li>
+        Private corporation
+      </li>
+      <li>
+        Pirates
+      </li>
+      <li>
+        Organized crime
+      </li>
+      <li>
+        Citizens
+      </li>
+      <li>
+        Other
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1398363023199" ID="ID_1356812659" MODIFIED="1403895497200">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Size and shape
+    </p>
+    <ul>
+      <li>
+        Bulky
+      </li>
+      <li>
+        Heavy
+      </li>
+      <li>
+        Volatile
+      </li>
+      <li>
+        Delicate
+      </li>
+      <li>
+        Light
+      </li>
+      <li>
+        Small
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1398281497786" ID="ID_1279523610" MODIFIED="1398449567753" TEXT="Merchant Goods">
+<node COLOR="#111111" CREATED="1398281505001" ID="ID_448611308" MODIFIED="1398449567754" TEXT="Food"/>
+<node COLOR="#111111" CREATED="1398281509128" ID="ID_515943121" MODIFIED="1398449567754" TEXT="Home Appliances"/>
+<node COLOR="#111111" CREATED="1398281511432" ID="ID_1218874098" MODIFIED="1398449567754" TEXT="Clothing"/>
+<node COLOR="#111111" CREATED="1398281512616" ID="ID_893269212" MODIFIED="1398449567754" TEXT="Vehicles"/>
+<node COLOR="#111111" CREATED="1398281513800" ID="ID_1308443000" MODIFIED="1398449567754" TEXT="Scrap metal"/>
+<node COLOR="#111111" CREATED="1398281515865" ID="ID_1683417577" MODIFIED="1398449567755" TEXT="Computer Parts"/>
+<node COLOR="#111111" CREATED="1398281518184" ID="ID_152794795" MODIFIED="1398449567755" TEXT="Machine Parts"/>
+<node COLOR="#111111" CREATED="1398281522168" ID="ID_691487420" MODIFIED="1398449567755" TEXT="Electronics"/>
+</node>
+<node COLOR="#111111" CREATED="1398281524840" ID="ID_1363739460" MODIFIED="1398449567755" TEXT="People">
+<node COLOR="#111111" CREATED="1398281531272" ID="ID_1707096673" MODIFIED="1398449567755" TEXT="Dignitaries"/>
+<node COLOR="#111111" CREATED="1398281533529" ID="ID_30763935" MODIFIED="1398449567756" TEXT="Messangers"/>
+<node COLOR="#111111" CREATED="1398281539449" ID="ID_817218015" MODIFIED="1398449567756" TEXT="Families"/>
+<node COLOR="#111111" CREATED="1398281541913" ID="ID_1370194399" MODIFIED="1398449567756" TEXT="Refugees"/>
+<node COLOR="#111111" CREATED="1398281707937" ID="ID_1093233821" MODIFIED="1398449567756" TEXT="Vacationers"/>
+</node>
+<node COLOR="#111111" CREATED="1398281548330" ID="ID_299132682" MODIFIED="1398449567756" TEXT="Finery">
+<node COLOR="#111111" CREATED="1398281565675" ID="ID_423772809" MODIFIED="1398449567756" TEXT="Designer clothes"/>
+<node COLOR="#111111" CREATED="1398281568858" ID="ID_1015230125" MODIFIED="1398449567757" TEXT="Jewelry"/>
+<node COLOR="#111111" CREATED="1398281571130" ID="ID_1441360170" MODIFIED="1398449567757" TEXT="Fine fabrics"/>
+<node COLOR="#111111" CREATED="1398281574618" ID="ID_1040247202" MODIFIED="1398449567757" TEXT="Luxury goods"/>
+</node>
+<node COLOR="#111111" CREATED="1398281550745" ID="ID_615715441" MODIFIED="1398449567757" TEXT="Volatile &amp; Fragile Cargo">
+<node COLOR="#111111" CREATED="1398281625500" ID="ID_195593385" MODIFIED="1398449567757" TEXT="Petroleum Fuel"/>
+<node COLOR="#111111" CREATED="1398281633165" ID="ID_1032119167" MODIFIED="1398449567758" TEXT="Compressed Gasses"/>
+<node COLOR="#111111" CREATED="1398281649534" ID="ID_1373306404" MODIFIED="1398449567758" TEXT="Starship Fuel"/>
+<node COLOR="#111111" CREATED="1398281677599" ID="ID_318124829" MODIFIED="1398449567758" TEXT="Explosives"/>
+</node>
+<node COLOR="#111111" CREATED="1398364019257" ID="ID_1894320440" MODIFIED="1398449567758" TEXT="Fragile Cargo">
+<node COLOR="#111111" CREATED="1398281642701" ID="ID_266894885" MODIFIED="1398449567758" TEXT="Scientific Equipment"/>
+<node COLOR="#111111" CREATED="1398281681279" ID="ID_527772048" MODIFIED="1398449567759" TEXT="Advanced Laser Lenses"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1403895535472" ID="ID_1813434534" MODIFIED="1403895550749" TEXT="A value multiplier will be assigned to each point">
+<node CREATED="1403895675654" ID="ID_1474486990" MODIFIED="1403895697364" TEXT="A primary balance point for the game. A spreadsheet is needed."/>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ Trunk/Documentation/FlowChart_Travel.mm
@@ -0,0 +1,59 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1403895730777" ID="ID_159589057" MODIFIED="1403895734677" TEXT="Travel">
+<node CREATED="1403895739786" ID="ID_1119002233" MODIFIED="1403895743658" POSITION="right" TEXT="Dock Screen">
+<node CREATED="1403895743659" ID="ID_1297243940" MODIFIED="1403895758886" TEXT="Intrasystem travel">
+<node CREATED="1403895760665" ID="ID_1476186922" MODIFIED="1403895771098" TEXT="Show system map">
+<node CREATED="1403897060051" ID="ID_1359845729" MODIFIED="1403897081232" TEXT="Map will highlight planets/stations/ships with active contracts "/>
+<node CREATED="1403896027637" ID="ID_1072994556" MODIFIED="1403896038898" TEXT="Route plot options:">
+<node CREATED="1403895771098" ID="ID_453970712" MODIFIED="1403895804137" TEXT="Click/Tap desired destination">
+<node CREATED="1403895840093" ID="ID_1037509363" MODIFIED="1403896383672" TEXT="Automatically calculate route (plot a route, fuel consumption, time)"/>
+</node>
+<node CREATED="1403895865999" ID="ID_676656206" MODIFIED="1403895891980" TEXT="Click/Tap non-docking objects to place Nav Points">
+<node CREATED="1403895894416" ID="ID_602477475" MODIFIED="1403896379169" TEXT="After each placement it will calculate the shortest route between the two points (plot a route, fuel consumption, time)"/>
+</node>
+<node CREATED="1403895805004" ID="ID_719706862" MODIFIED="1403895818265" TEXT="Click and drag to create a custom route">
+<node CREATED="1403895856094" ID="ID_1133849577" MODIFIED="1403896367706" TEXT="Once released it will calculate the route traced (fuel consumption time)"/>
+</node>
+</node>
+<node CREATED="1403895823293" ID="ID_676932487" MODIFIED="1403895837626" TEXT="As the mouse moves further from the destination the solar system will rotate">
+<node CREATED="1403895998020" ID="ID_1047845713" MODIFIED="1403896012818" TEXT="Once finished click &quot;Go&quot; &quot;Launch&quot; or the like"/>
+<node CREATED="1403896013317" ID="ID_570987421" MODIFIED="1403896022882" TEXT="To revise click &quot;Clear&quot; or &quot;Delete last&quot; or the like"/>
+</node>
+</node>
+<node CREATED="1403895750137" ID="ID_1844091934" MODIFIED="1403896287869" TEXT="Navigate to...">
+<node CREATED="1403896078807" ID="ID_1935505519" MODIFIED="1403896291997" TEXT="a jump beacon">
+<node CREATED="1403896299809" ID="ID_353860212" MODIFIED="1403896308001" TEXT="&quot;Dock&quot; with the beacon">
+<node CREATED="1403897088501" ID="ID_1968397471" MODIFIED="1403897104018" TEXT="Map will highlight Beacons in systems with active contracts"/>
+<node CREATED="1403896308002" ID="ID_1457315607" MODIFIED="1403896406423" TEXT="Show Beacon map">
+<node CREATED="1403896322288" ID="ID_184963630" MODIFIED="1403896462340" TEXT="Choose jump beacon">
+<node CREATED="1403896331569" ID="ID_1692942103" MODIFIED="1403896451049" TEXT="Calculate route (fuel consumption, time)">
+<node CREATED="1403896451049" ID="ID_1815670621" MODIFIED="1403896452421" TEXT="Engage!"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1403896292225" ID="ID_1389368378" MODIFIED="1403896297037" TEXT="a designated Warp zone">
+<node CREATED="1403896393595" ID="ID_679068258" MODIFIED="1403896397420" TEXT="Engage warp drive">
+<node CREATED="1403897111621" ID="ID_761800077" MODIFIED="1403897126371" TEXT="Map will highlight systems with active contracts"/>
+<node CREATED="1403896397421" ID="ID_1891317060" MODIFIED="1403896402582" TEXT="Show galaxy map">
+<node CREATED="1403896409691" ID="ID_1619652787" MODIFIED="1403897141482" TEXT="Choose a destination star">
+<node CREATED="1403895771098" ID="ID_999213734" MODIFIED="1403895804137" TEXT="Click/Tap desired destination">
+<node CREATED="1403895840093" ID="ID_1205262809" MODIFIED="1403896383672" TEXT="Automatically calculate route (plot a route, fuel consumption, time)"/>
+</node>
+<node CREATED="1403895865999" ID="ID_1141016383" MODIFIED="1403895891980" TEXT="Click/Tap non-docking objects to place Nav Points">
+<node CREATED="1403895894416" ID="ID_17365065" MODIFIED="1403896379169" TEXT="After each placement it will calculate the shortest route between the two points (plot a route, fuel consumption, time)"/>
+</node>
+<node CREATED="1403895805004" ID="ID_666208848" MODIFIED="1403895818265" TEXT="Click and drag to create a custom route">
+<node CREATED="1403895856094" ID="ID_1489523605" MODIFIED="1403896367706" TEXT="Once released it will calculate the route traced (fuel consumption time)"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
